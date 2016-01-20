e69850ff48593a8f0cd0567c80b6bb793304c0ac
mguryanov <taychi@inbox.ru>
2016-01-20 13:28:39 +0500

    chgs: algorithms : lesson 3 : ex 3

--- lessons-algorithms/test-l3e3.pl
+++ lessons-algorithms/test-l3e3.pl
@@ -10,8 +10,9 @@ use experimental qw/smartmatch/;
 use Data::Dumper;
 use IPC::Open2;
 use Term::ANSIColor qw(:constants);
+use Time::HiRes qw(usleep nanosleep);
 
-use constant REQS_LIMIT => 10;
+use constant REQS_LIMIT => 1;
 
 my ($fhread, $fhwrite, $pid, $result);
 
@@ -19,11 +20,12 @@ my ($fhread, $fhwrite, $pid, $result);
 {
     for ( my $i=0; $i<REQS_LIMIT; ++$i ) {
         my $test=generate_test();
-        say qq{ test :\n}.$test.qq{\n};
-        say qq{ result :\n}.Dumper($result).qq{\n};
-#        post_test( $test );
-#        check_test( $test );
-#        finalize ();
+#        say qq{ test :\n}.$test.qq{\n};
+#        say qq{ result :\n}.$result.qq{\n};
+#        usleep( 100000 );
+        post_test( $test );
+        check_test( $test );
+        finalize ();
     }
 }
 
@@ -39,43 +41,59 @@ sub generate_test {
         push @$segments,[$init_point,$segment_length];
     }
 
-    #$point_number=19;
-    #$list = [[2,36],[84,46],[4,63],[42,15],[31,43],[20,57],[21,68],
-    #         [25,14],[24,89],[9,52],[33,62],[82,43],[32,54],[78,21],[67,0],
-    #         [81,82],[31,22],[59,76],[92,10]];
+#    $segment_number=10;
+#    $segments = [[27,20],[47,16],[51,31],[59,3],[67,15],
+#                 [69,88],[73,22],[79,9],[83,45],[85,49]];
 
     my $req_str=$segment_number.qq{\n}.
                 join( qq{\n}, map { $_->[0].q{ }.$_->[1] } @$segments );
 
     my $tmp=[ sort { $a->[0] <=> $b->[0] }
-              sort { $a->[1] <=> $b->[1] } @$list ];
+              sort { $a->[1] <=> $b->[1] } @$segments ];
 
-    for( my $i1=0, $i2=1; $i2<$segment_number-1; ++$i2 )
+    #say Dumper( $tmp );
+
+    unless( $segment_number ) {
+        $result=0;
+        return $req_str;
+    }
+
+    $result=$tmp->[0]->[1];
+    my $length=0;
+
+    for( my $i1=0, my $i2=1; $i2<$segment_number; )
     {
-        # summ all length
-        $result+=$segments->[$i1]->[1];
-        my $y=( $segments->[$i1]->[0] + $segments->[$i1]->[1] );
-        # greater and equal guaranteed by sort => not check
-        if( $segments->[$i1]->[0] < $segments->[$i2]->[0] ) {
-            if( $y =< $segments->[$i2]->[0] ) {
-                ++$i1;
-                continue;
-            }
-            my $white=$segments->[i+1]->[0] - $segments->[i]->[0];
-            my $next_y=$segments->[i+1]->[0] + $segments->[i+1]->[1];
-            if( $y < $next_y ) {
-                $result-=$y-$segments->[i+1]->[0];
-            } else {
-                $result-=$segments->[i+1]->[1];
-            }
+        my $y1=( $tmp->[$i1]->[0] + $tmp->[$i1]->[1] );
+        my $y2=$tmp->[$i2]->[0] + $tmp->[$i2]->[1];
+        if( $tmp->[$i1]->[0] >= $y2 ) {
+            ++$i2;$length=0;
         }
-
-        $result-=$segments->[$i1]->[1];
-        $segments->[$i2]->[0]=$y;
-        $segments->[$i2]->[1]-=$y-$segments->[$i2]->[0];
-        ++$i1;
-
-        ++$lli if( $segments->[$lli]->[1] =< 0 );
+        # greater and equal guaranteed by sort => not check
+        elsif( $y1 <= $tmp->[$i2]->[0] ) {
+            $length=$tmp->[$i2]->[1];
+            $i1=$i2++;
+        }
+        elsif( $y1 < $y2 ) {
+            if( $tmp->[$i1]->[0] < $tmp->[$i2]->[0] ) {
+                $result-=$y1-$tmp->[$i2]->[0];
+            } else {
+                $result-=$tmp->[$i1]->[1];
+            }
+            $tmp->[$i2]->[1]=$y2-$y1;
+            $tmp->[$i2]->[0]=$y1;
+            $length=$tmp->[$i2]->[1];
+            $i1=$i2++;
+        } else {
+            if( $tmp->[$i1]->[0] < $tmp->[$i2]->[0] ) {
+                $result-=$tmp->[$i2]->[1];
+            } else {
+                $result-=$y2-$tmp->[$i1]->[0];
+            }
+            $tmp->[$i1]->[1]=$y1-$y2;
+            $tmp->[$i1]->[0]=$y2;
+            ++$i2;$length=0;
+        }
+        $result+=$length;
     }
 
     return $req_str;
@@ -87,7 +105,7 @@ sub post_test {
     my($test,$fh)=@_;
     $pid=open2(
         $fhread, $fhwrite,
-        q{/home/maxim/app/my/blogspot-examples/sandbox-build/algorithms/l3e2}
+        q{/home/maxim/app/my/blogspot-examples/sandbox-build/algorithms/l3e3}
     );
     syswrite $fhwrite,$test,length($test);
     close $fhwrite;
@@ -113,8 +131,7 @@ sub check_test {
     unless ( $r ~~ $result ) {
         say RED qq{failed response : \n}.
                 join( qq{\n}, map { $_->[0].q{ }.$_->[1] } @$r ).qq{\n}.
-                q{ must be : }.
-                join( qq{\n}, map { $_->[0].q{ }.$_->[1] } @$result ).qq{\n}.
+                q{ must be : }.$result.qq{\n}.
                 q{ test : }.$test;
         return;
     }
