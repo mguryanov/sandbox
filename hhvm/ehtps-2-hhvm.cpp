712f142a76adfe5d38f14035b59929d92546f917
mguryanov <taychi@inbox.ru>
2016-02-03 13:42:52 +0500

    hhvm: experiment : ehtps-2-hhvm : up

--- hhvm/ehtps-2-hhvm.cpp
+++ hhvm/ehtps-2-hhvm.cpp
@@ -1,139 +1,271 @@
-#include "zend-html.h"
 #include "test-html-entities.h"
 #include <iostream>
 #include <iomanip>
+#include <cstring>
+#include <cassert>
+#include <deque>
 
 using namespace std;
+using namespace HPHP;
+
+
+bool
+test_encode_sentence(
+        const char* sentence,
+        entity_charset charset,
+        HPHP::EntBitmask doc )
+{
+    int64_t flag=static_cast<int64_t>( doc )                        |
+                 static_cast<int64_t>( EntBitmask::ENT_BM_IGNORE )  |
+                 static_cast<int64_t>( EntBitmask::ENT_BM_SINGLE )  |
+                 static_cast<int64_t>( EntBitmask::ENT_BM_DOUBLE );
+
+    int len=std::char_traits<char>::length( sentence );
+    char* encoded_response=HPHP::string_html_encode(
+                                        sentence,
+                                        len,flag,
+                                        charset,
+                                        true,
+                                        true
+                                   );
+
+    cout << "\033[1;32m"
+         << encoded_response
+         << "\033[0m" << endl;
+
+    return true;
+}
+
+
+bool
+test_encode_entities(
+        const char* header,
+        pair<const char*,test_case_meta_t>* row,
+        vector<pair<const char*,const char*>>& result,
+        deque<pair<size_t,size_t>>& pos,
+        bool all)
+{
+    cout << header << row->first << " ";
+
+    int64_t flag=static_cast<int64_t>( row->second.doc )            |
+                 static_cast<int64_t>( EntBitmask::ENT_BM_SINGLE ) |
+                 static_cast<int64_t>( EntBitmask::ENT_BM_DOUBLE );
+
+    entity_test_case_t* table=
+            reinterpret_cast<entity_test_case_t*>( row->second.ptr );
+
+    size_t begin = result.size();
+    size_t end = begin;
+
+    for( auto v : *table )
+    {
+        if( !v.second )
+            continue;
+
+        int len=std::char_traits<char>::length( v.first );
+        char* encoded_response=HPHP::string_html_encode(
+                                            v.first,len,flag,
+                                            row->second.charset,
+                                            true,
+                                            all
+                                       );
+
+        int offset=( len == 1 ) ? 0 : 1;
+        len=std::char_traits<char>::length( v.second );
+
+        if( strncmp( encoded_response+offset, v.second, len ) != 0 ) {
+            if( all ) {
+                cout << setfill(' ') << setw(25) << "\033[1;31m" <<
+                        encoded_response << "\t" <<
+                        v.second << "\033[0m" << endl;
+                return false;
+            }
+        }
+
+        else {
+            result.push_back(
+                        pair<const char*,const char*>(
+                                encoded_response,
+                                v.first
+                            )
+                    );
+            ++end;
+        }
+    }
+
+    pos.push_back( pair<size_t,size_t>( begin, --end ));
+    cout << "\033[1;32mOk!\033[0m" << endl;
+
+    return true;
+}
+
+
+
+bool
+test_decode_entities(
+        const char* header,
+        pair<const char*,test_case_meta_t>* row,
+        vector<pair<const char*,const char*>>& result,
+        deque<pair<size_t,size_t>>& pos,
+        bool all)
+{
+    cout << header << row->first << " ";
+
+    int64_t flag=static_cast<int64_t>( row->second.doc )           |
+                 static_cast<int64_t>( EntBitmask::ENT_BM_SINGLE ) |
+                 static_cast<int64_t>( EntBitmask::ENT_BM_DOUBLE );
+
+    bool bad_case=false;
+    pair<size_t,size_t> p=pos[0];
+    pos.pop_front();
+
+    for( int i = p.first; i <= p.second; ++i )
+    {
+        int len=std::char_traits<char>::length( result[i].first );
+        char* decoded_response=HPHP::string_html_decode(
+                                            result[i].first,len,flag,
+                                            row->second.hint_charset,
+                                            all
+                                       );
+
+        len=std::char_traits<char>::length( result[i].second );
+
+        if( strncmp( decoded_response, result[i].second, len ) != 0 ) {
+            bad_case=true;
+            cout << setfill(' ') << setw(25) << "\033[1;31m" <<
+                    decoded_response << "\t" <<
+                    result[i].second << "\033[0m" << endl;
+        }
+
+        free( decoded_response );
+
+        if( bad_case ) {
+            return false;
+        }
+    }
+
+    cout << "\033[1;32mOk!\033[0m" << endl;
+    return true;
+}
+
+
+void __free(vector<pair<const char*,const char*>>& result)
+{
+    for( auto inst : result ) {
+        free( const_cast<char*>( inst.first ) );
+    }
+
+    return;
+}
+
+
 
 int main()
 {
-    int64_t flag=static_cast<int64_t>( HPHP::EntBitmask::ENT_BM_XHTML );
-    cout << "htmlentities: cp1251_xhtml " << endl;
-/*
-    for( auto v : cp1251_html401 )
-    {
-        int len=sizeof( v.first );
-        char* response_htmlentities=HPHP::string_html_encode(
-                                                v.first,len,flag,
-                                                entity_charset::cs_cp1251,
-                                                true,
-                                                true
-                                           );
+     vector<pair<const char*,const char*>> result;
+     deque<pair<size_t,size_t>> pos;
 
-        cout << setfill(' ') << setw(25) <<
-                response_htmlentities << "\t" <<
-                v.second << endl;
+     // htmlentities
 
-        free( response_htmlentities );
+    for( auto v : test_single_cases ) {
+        if( test_encode_entities(
+                "htmlentities single encode: ",
+                &v, result, pos, true
+            ) == false )
+        {
+            __free( result );
+        }
     }
 
-    flag=static_cast<int64_t>( HPHP::EntBitmask::ENT_BM_HTML401 );
-    cout << "htmlentities: cp1251_html401 " << endl;
-
-    for( auto v : cp1251_html401 )
-    {
-        int len=sizeof( v.first );
-        char* response_htmlentities=HPHP::string_html_encode(
-                                                v.first,len,flag,
-                                                entity_charset::cs_cp1251,
-                                                true,
-                                                true
-                                           );
-
-        cout << setfill(' ') << setw(25) <<
-                response_htmlentities << "\t" <<
-                v.second << endl;
-
-        free( response_htmlentities );
+    for( auto v : test_single_cases ) {
+        if( test_decode_entities(
+                "htmlentities single decode: ",
+                &v, result, pos, true
+            ) == false )
+        {
+            __free( result );
+        }
     }
 
-    flag=static_cast<int64_t>( HPHP::EntBitmask::ENT_BM_HTML5 );
-    cout << "htmlentities: cp1251_html5 " << endl;
+    // htmlspecialchars
 
-    for( auto v : cp1251_html5 )
-    {
-        int len=sizeof( v.first );
-        char* response_htmlentities=HPHP::string_html_encode(
-                                                v.first,len,flag,
-                                                entity_charset::cs_cp1251,
-                                                true,
-                                                true
-                                           );
+    result.clear();
+    pos.clear();
 
-        cout << setfill(' ') << setw(25) <<
-                response_htmlentities << "\t" <<
-                v.second << endl;
-
-        free( response_htmlentities );
-    }
-    flag=static_cast<int64_t>( HPHP::EntBitmask::ENT_BM_XHTML );
-    cout << "htmlentities: utf8_xhtml " << endl;
-
-    for( auto v : utf8_xhtml )
-    {
-        int len=sizeof( v.first );
-        char* response_htmlentities=HPHP::string_html_encode(
-                                                v.first,len,flag,
-                                                entity_charset::cs_utf_8,
-                                                true,
-                                                true
-                                           );
-
-        cout << setfill(' ') << setw(25) <<
-                response_htmlentities << "\t" <<
-                v.second << endl;
-
-        free( response_htmlentities );
+    for( auto v : test_single_cases ) {
+        if( test_encode_entities(
+                "htmlspecialchars single encode: ",
+                &v, result, pos, false
+            ) == false )
+        {
+            __free( result );
+        }
     }
 
-    flag=static_cast<int64_t>( HPHP::EntBitmask::ENT_BM_HTML401 );
-    cout << "htmlentities: utf8_html401 " << endl;
-
-    for( auto v : utf8_html401 )
-    {
-        int len=sizeof( v.first );
-        char* response_htmlentities=HPHP::string_html_encode(
-                                                v.first,len,flag,
-                                                entity_charset::cs_utf_8,
-                                                true,
-                                                true
-                                           );
-
-        cout << setfill(' ') << setw(25) <<
-                response_htmlentities << "\t" <<
-                v.second << endl;
-
-        free( response_htmlentities );
-    }
-*/
-
-    flag=static_cast<int64_t>( HPHP::EntBitmask::ENT_BM_HTML5 );
-    cout << "htmlentities: utf8_html5 " << endl;
-
-    for( auto v : utf8_html5 )
-    {
-        int len=sizeof( v.first );
-        char* response_htmlentities=HPHP::string_html_encode(
-                                                v.first,len,flag,
-                                                entity_charset::cs_utf_8,
-                                                true,
-                                                true
-                                           );
-
-        cout << setfill(' ') << setw(25) <<
-                response_htmlentities << "\t" <<
-                v.second << endl;
-
-        free( response_htmlentities );
+    for( auto v : test_single_cases ) {
+        if( test_decode_entities(
+                "htmlspecialchars single decode: ",
+                &v, result, pos, false
+            ) == false )
+        {
+            __free( result );
+        }
     }
 
+    // text slot test
 
-//    char* response_htmlspecialchars=HPHP::string_html_encode(
-//                                                   str,len,flag,
-//                                                   entity_charset::cs_cp1251,
-//                                                   true,
-//                                                   false
-//                                               );
+    // HTML401
+
+    test_encode_sentence(
+                koi8r_sentence,
+                cs_koi8r,
+                EntBitmask::ENT_BM_HTML401
+            );
+
+    test_encode_sentence(
+                cp866_sentence,
+                cs_cp866,
+                EntBitmask::ENT_BM_HTML401
+            );
+
+    test_encode_sentence(
+                cp1251_sentence,
+                cs_cp1251,
+                EntBitmask::ENT_BM_HTML401
+            );
+
+    test_encode_sentence(
+                utf8_sentence,
+                cs_utf_8,
+                EntBitmask::ENT_BM_HTML401
+            );
+
+    test_encode_sentence(
+                koi8r_sentence,
+                cs_koi8r,
+                EntBitmask::ENT_BM_HTML5
+            );
+
+    // HTML5
+
+    test_encode_sentence(
+                cp866_sentence,
+                cs_cp866,
+                EntBitmask::ENT_BM_HTML5
+            );
+
+
+    test_encode_sentence(
+                cp1251_sentence,
+                cs_cp1251,
+                EntBitmask::ENT_BM_HTML5
+            );
+
+    test_encode_sentence(
+                utf8_sentence,
+                cs_utf_8,
+                EntBitmask::ENT_BM_HTML5
+            );
 
     return 0;
 }
